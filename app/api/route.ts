256c792e4b3e9e61857cd187a6dc85f1391b5b8a
Hasnaat Iftikhar <hasnaatfreelancing@gmail.com>
2023-08-16 18:31:01 +0500

    feat: Worked on routing

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,7 @@
 import { getAuthSession } from "@/lib/auth";
+import { prisma } from "@/lib/db";
 import { ProductFormValidator } from "@/lib/validators/productForm";
+import { z } from "zod";
 
 export async function POST(req: Request) {
   try {
@@ -13,5 +15,26 @@ export async function POST(req: Request) {
     const body = req.json();
     const { name, image, description, category, brand, price, tags } =
       ProductFormValidator.parse(body);
-  } catch (err) {}
+
+    await prisma.product.create({
+      data: {
+        name,
+        image,
+        description,
+        category,
+        brand,
+        price,
+        tags,
+      },
+    });
+
+    return new Response("Product created successfully!");
+  } catch (err) {
+    if (err instanceof z.ZodError)
+      return new Response(err.message, {
+        status: 422,
+      });
+
+    return new Response("Could not create product", { status: 500 });
+  }
 }
